f5e9bf645d9858d667fca0cf4481ccd00e84784f
liudidi <liudidi@gmail.com>
2011-11-16 14:55:20 -0500

    添加 emacs

---
+++ emacs/dotemacs.el
@@ -0,0 +1,17 @@
+;; .emacs
+
+;;; uncomment this line to disable loading of "default.el" at startup
+;; (setq inhibit-default-init t)
+
+;; default to better frame titles
+(setq frame-title-format
+      (concat  "%b - emacs@" (system-name)))
+
+;; default to unified diffs
+(setq diff-switches "-u")
+
+;; always end a file with a newline
+;(setq require-final-newline 'query)
+
+;;; uncomment for CJK utf-8 support for non-Asian users
+;; (require 'un-define)
